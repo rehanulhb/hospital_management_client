d6f2656c116f62944e468fae8ed4538f2a114e55
rehanulhb <rehanulh@gmail.com>
2026-02-23 01:04:04 +0100

    Added Error and Success Toast for Register, Login and Logout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "sonner";
 import "./globals.css";
+import LoginSuccessToast from "@/components/shared/LoginSuccessToast";
+import LogoutSuccessToast from "@/components/shared/LogoutSuccessToast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,8 +16,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Hospital Management System",
-  description: "Hospital Management System built by Rehanul Hasan Bappy",
+  title: "PH-Health-Care",
+  description: "A healthcare application built with Next.js",
 };
 
 export default function RootLayout({
@@ -28,6 +31,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Toaster position="top-right" richColors />
+        <LoginSuccessToast />
+        <LogoutSuccessToast />
       </body>
     </html>
   );
